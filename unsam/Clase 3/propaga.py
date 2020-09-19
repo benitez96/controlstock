57318376b064f347377a51a559d390eb4e2e5eb2
daniel-pc-escritorio <danii-benitez@hotmail.com>
2020-09-19 09:43:56 -0300

    modifico

--- unsam/Clase 3/propaga.py
+++ unsam/Clase 3/propaga.py
@@ -1,10 +1,13 @@
 def propagar(lista):
-    for i in range(len(lista)-1):
+    for i in range(len(lista)):
         if lista[i] == 1:
-            if lista[i-1] == 0:
-                lista[i-1] = 1
-                propagar(lista)
-            elif lista[i+1] == 0:
-                lista[i+1] = 1
-                propagar(lista)
+            try:
+                if lista[i-1] == 0:
+                    lista[i-1] = 1
+                    propagar(lista)
+                elif lista[i+1] == 0:
+                    lista[i+1] = 1
+                    propagar(lista)
+            except IndexError:
+                continue
     return lista
