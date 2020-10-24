5648b8cd5017301849021087c1b775b10b231dd5
daniel-pc-escritorio <danii-benitez@hotmail.com>
2020-10-24 18:29:48 -0300

    fin version 1.0

--- CONTROLSTOCK/principal.py
+++ CONTROLSTOCK/principal.py
@@ -17,18 +17,18 @@ def ir_stock():
     topl = Tk()
     gui_productos.CargaProductos(topl)
     topl.mainloop()
-    
-ventana = Tk()
-ventana.config(bg='#728fce')
-ventana.geometry('800x400')
-ventana.title('GESTION CASA ELECTRODOMESTICOS')
-Button(ventana, text='REALIZAR\nVENTA', font=('Verdana', 15), command=ir_ventas).place(x= 230, y=250)
-Button(ventana, text='GESTION\nCLIENTES', font=('Verdana', 15), command=ir_clientes).place(x=360, y=250)
-Button(ventana, text='CONTROL\nSTOCK', font=('Verdana', 15), command=ir_stock).place(x=490, y=250)
-logo = PhotoImage(file='electro.gif')
-Label(ventana, image=logo).place(x=60, y=100)
+
+if __name__=='__main__':
+
+    ventana = Tk()
+    ventana.config(bg='#728fce')
+    ventana.geometry('800x400')
+    ventana.title('GESTION CASA ELECTRODOMESTICOS')
+    Button(ventana, text='REALIZAR\nVENTA', font=('Verdana', 15), command=ir_ventas).place(x= 230, y=250)
+    Button(ventana, text='GESTION\nCLIENTES', font=('Verdana', 15), command=ir_clientes).place(x=360, y=250)
+    Button(ventana, text='CONTROL\nSTOCK', font=('Verdana', 15), command=ir_stock).place(x=490, y=250)
+    logo = PhotoImage(file='.\\electro.gif')
+    Label(ventana, image=logo).place(x=60, y=100)
 
 
-
-
-ventana.mainloop()
+    ventana.mainloop()
