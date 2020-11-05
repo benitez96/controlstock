e257980b76274eda6f16fe24ac40865ab9324e4f
benitez96 <danii-benitez@hotmail.com>
2020-11-05 19:10:00 -0300

    resumen

--- CONTROLSTOCK/resumen.py
+++ CONTROLSTOCK/resumen.py
@@ -1,35 +1,37 @@
 import sqlite3
 import pandas as pd
 import seaborn as sns
-import tkinter
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#import tkinter
+#import matplotlib.pyplot as plt
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 with sqlite3.connect('controlstock') as db:
 
     df_ventas = pd.read_sql_query('SELECT * FROM VENTAS', db)
     df_productos = pd.read_sql_query('SELECT * FROM PRODUCTOS', db)
+    
+sns.catplot(data=df_ventas, x='fecha_venta', y='precio_venta', kind='bar')
 
 
-def create_plot():
-    #sns.set(style="white")
+# def create_plot():
+#     #sns.set(style="white")
 
     
-    # Set up the matplotlib figure
-    f, ax = plt.subplots(figsize=(5, 5))
+#     # Set up the matplotlib figure
+#     f, ax = plt.subplots(figsize=(5, 5))
     
-    sns.displot(data=df_ventas, x='fecha_venta', y='precio_venta')
-    return f
+#     sns.displot(data=df_ventas, x='fecha_venta', y='precio_venta')
+#     return f
 
 
 
 
 
-root = tkinter.Tk()
+# root = tkinter.Tk()
 
-fig = create_plot()
-canvas = FigureCanvasTkAgg(fig, master=root)    #
-canvas.draw()
-canvas.get_tk_widget().pack()
+# fig = create_plot()
+# canvas = FigureCanvasTkAgg(fig, master=root)    #
+# canvas.draw()
+# canvas.get_tk_widget().pack()
 
-root.mainloop()
+# root.mainloop()
